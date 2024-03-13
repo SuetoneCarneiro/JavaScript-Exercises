ba2708e6a091043cbfcbf9fa3ee66101c01a2d18
Suetone Carneiro <suetoneneto12@gmail.com>
2024-03-12 21:34:01 -0300

    Products exercise

---
+++ products-exercise/src/pages/exercises/array-products-qtd/_codes/js/code/products-qtd.js
@@ -0,0 +1,13 @@
+export function gtZero(products) {
+  return products.filter((products) => products[2] > 0)
+}
+
+export function subTotal(products) {
+  const sum = (product) => [product[0], product[1]*product[2]] // arrow function
+  return products.map(sum)
+}
+
+export function total(products) {
+  const sum = (total, product) => total + product[1] * product[2]
+  return products.reduce(sum,0)
+}
