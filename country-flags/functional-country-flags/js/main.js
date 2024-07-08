0c36c3a5b0765f154dcc8b8c0d518279f6ee8e95
Suetone Carneiro <suetoneneto12@gmail.com>
2024-07-08 15:08:07 -0300

    fix: adding a join to the map

--- country-flags/functional-country-flags/js/main.js
+++ country-flags/functional-country-flags/js/main.js
@@ -6,6 +6,6 @@ let flags_mapped = flags.map((country) =>
     `<div class="flag col-2 my-2 text-center">
        <img src="https://cdn.jsdelivr.net/gh/hampusborgos/country-flags@main/svg/${country.id}.svg" alt="${country.name}">
        <p>${country.name}</p>
-    </div>`);
+    </div>`).join('');
 
 main.insertAdjacentHTML('beforeend', flags_mapped);
