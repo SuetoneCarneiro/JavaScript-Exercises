f6b0542c735e0fbd1bda6424152fa793dafb4a9b
Suetone Carneiro <suetoneneto12@gmail.com>
2024-07-08 10:05:20 -0300

    feat: functional country-flags code

---
+++ country-flags/functional-country-flags/js/main.js
@@ -0,0 +1,11 @@
+import flags from './model/flags.js';
+
+const main = document.querySelector('main.row');
+
+let flags_mapped = flags.map((country) => 
+    `<div class="flag col-2 my-2 text-center">
+       <img src="https://cdn.jsdelivr.net/gh/hampusborgos/country-flags@main/svg/${country.id}.svg" alt="${country.name}">
+       <p>${country.name}</p>
+    </div>`);
+
+main.insertAdjacentHTML('beforeend', flags_mapped);
