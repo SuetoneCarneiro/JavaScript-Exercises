d8ff99726da6bed1c336fb938da8773773e9f030
Suetone Carneiro <suetoneneto12@gmail.com>
2024-09-02 17:51:43 -0300

    base do formulario e api viacep

---
+++ api-via-cep/js/main.js
@@ -0,0 +1,6 @@
+const cep = document.querySelector('#cep');
+const bairro = document.querySelector('#neighborhood');
+const rua = document.querySelector('#street');
+const numero = document.querySelector('#number');
+const cidade = document.querySelector('#city');
+const estado = document.querySelector('#state');
